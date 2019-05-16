80884aa3539c9c0e54e52fbb4ade1aecee455cda
win10 <554320535@qq.com>
2019-05-16 15:26:31 +0800

    添加项目文件。

---
+++ WebApplication4/Models/MyDbInit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class MyDbInit
+         : System.Data.Entity.CreateDatabaseIfNotExists<MyDb>
+    {
+        protected override void Seed(MyDb context)
+        {
+            base.Seed(context);
+
+
+            context.Xueshengxinxis.Add(new Xueshengxinxi
+            {
+                Xueshengxuehao = "20180001",
+                Xueshengxingming = "张三",
+                Xueshengxingbie = "男"
+
+            });
+            context.Xueshengxinxis.Add(new Xueshengxinxi
+            {
+                Xueshengxuehao = "20180002",
+                Xueshengxingming = "李四",
+                Xueshengxingbie = "女"
+            });
+
+
+        }
+
+
+    }
+}
