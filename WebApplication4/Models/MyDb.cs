a1ab29b7881a2b075679b802d0f8f272742dcbd8
大神 陈 <554320535@qq.com>
2019-05-16 16:53:21 +0800

    asd

---
+++ WebApplication4/Models/MyDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<WebApplication4.Models.Xueshengxinxi> Xueshengxinxis { get; set; }
+
+        public System.Data.Entity.DbSet<WebApplication4.Models.Chengji> Chengjis { get; set; }
+    }
+}
